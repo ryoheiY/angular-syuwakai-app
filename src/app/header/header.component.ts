33d9a06516d3952b2c327939b36aec8601f0d1c8
ryoheiY <momoyamashia@icloud.com>
2024-07-28 22:05:27 +0900

    linkがアクティブな時にヘッダーのリンクの文字色が変わるように修正

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
-import {RouterLink} from "@angular/router";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
