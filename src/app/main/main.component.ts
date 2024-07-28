7caaad071f6377a1f09ce72cab1dbd9db16df426
ryoheiY <momoyamashia@icloud.com>
2024-07-28 23:58:44 +0900

    Home画面のリンクを追加

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,24 +1,27 @@
 import {Component} from '@angular/core';
 import {CardComponent} from "./ui/card/card.component";
+import {RouterLink} from "@angular/router";
 
 interface HeaderMenu {
   title: string;
   description: string;
+  link: string | null;
 }
 
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [
-    CardComponent
+    CardComponent,
+    RouterLink
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
   headerList: HeaderMenu[] = [
-    {title: '一覧', description: 'ユーザ一覧、スピーカー、ファシリテーターを確認できる一覧画面です。'},
-    {title: '工事中', description: '工事中です。'},
+    {title: '一覧', description: 'ユーザ一覧、スピーカー、ファシリテーターを確認できる一覧画面です。', link: 'list'},
+    {title: '工事中', description: '工事中です。', link: null},
   ];
 
 }
